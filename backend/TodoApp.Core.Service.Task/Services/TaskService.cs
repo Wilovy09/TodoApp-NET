622c59cd948e64c247ca9ec2098908bbdb712a80
Emiliano Maldonado Garza <SrWilovy09@gmail.com>
2025-01-28 16:14:24 -0600

    :bug:

--- backend/TodoApp.Core.Service.Task/Services/TaskService.cs
+++ backend/TodoApp.Core.Service.Task/Services/TaskService.cs
@@ -22,8 +22,6 @@ public class TaskService : Proto.TaskManager.TaskManagerBase
 
         var taskList = new Proto.TaskList();
 
-        // E Argument 1: cannot convert from 'System.Collections.Generic.List<TodoApp.Core.Protos.Task.RawTask>'
-        // to 'System.Collections.Generic.IEnumerable<TodoApp.Core.Protos.Task.RawTask>' csharp (CS1503) [30, 33]
         taskList.Tasks.Add(
             tasks.Select(t => new Proto.RawTask
             {
@@ -36,5 +34,57 @@ public class TaskService : Proto.TaskManager.TaskManagerBase
         );
 
         return Tasks.Task.FromResult(taskList);
+    }
+
+    public override Tasks.Task<Proto.RawTask> Find(
+        Proto.TaskUuid request,
+        ServerCallContext context
+    )
+    {
+        var tasksRepository = new TaskRepositorie();
+        var task = tasksRepository.FindTask(request.Uuid);
+        return Tasks.Task.FromResult(
+            new Proto.RawTask
+            {
+                Uuid = task.Uuid,
+                Title = task.Title,
+                TaskId = task.TaskId,
+                Description = task.Description,
+                Complete = task.Complete,
+            }
+        );
+    }
+
+    public override Tasks.Task<Proto.RawTask> Create(
+        Proto.CreateTask request,
+        ServerCallContext context
+    )
+    {
+        var tasksRepository = new TaskRepositorie();
+        var createTask = new Proto.PartialTask
+        {
+            Uuid = tasksRepository.GenerateUUID(),
+            Title = request.Title,
+            Description = request.Description,
+            Complete = request.Complete,
+        };
+
+        // /home/wilovy/Dev/TodoApp/backend/TodoApp.Core.Service.Task/Services/TaskService.cs(72,47):
+        // error CS1503: Argument 1: cannot convert from 'TodoApp.Core.Protos.Task.PartialTask
+        // [/home/wilovy/Dev/TodoApp/backend/TodoApp.Core.Service.Task/obj/Debug/net8.0/Task.cs(989)]'
+        // to 'TodoApp.Core.Protos.Task.PartialTask [/home/wilovy/Dev/TodoApp/backend/TodoApp.Core.Infraestructure/obj/Debug/net8.0/ref/TodoApp.Core.Infraestructure.dll]'
+        // [/home/wilovy/Dev/TodoApp/backend/TodoApp.Core.Service.Task/TodoApp.Core.Service.Task.csproj]
+        var task = tasksRepository.CreateTask(createTask);
+
+        return Tasks.Task.FromResult(
+            new Proto.RawTask
+            {
+                Uuid = task.Uuid,
+                TaskId = task.TaskId,
+                Title = task.Title,
+                Description = task.Description,
+                Complete = task.Complete,
+            }
+        );
     }
 }
